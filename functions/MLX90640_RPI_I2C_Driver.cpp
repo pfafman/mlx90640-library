b0c3f495054d01e8f3d21b9617f27321de3d8334
Timothy Pfafman <tim@pfafman.com>
2018-10-28 08:39:20 -0600

    debug

--- functions/MLX90640_RPI_I2C_Driver.cpp
+++ functions/MLX90640_RPI_I2C_Driver.cpp
@@ -27,8 +27,9 @@ void MLX90640_I2CInit()
 
 int MLX90640_I2CRead(uint8_t slaveAddr, uint16_t startAddress, uint16_t nMemAddressRead, uint16_t *data)
 {
+    printf("MLX90640_I2CRead\n");
     if (!init) {
-        printf("bcm2835_ini\n");
+        printf("bcm2835_init\n");
         bcm2835_init();
     	bcm2835_i2c_begin();
         bcm2835_i2c_set_baudrate(400000);
@@ -39,6 +40,7 @@ int MLX90640_I2CRead(uint8_t slaveAddr, uint16_t startAddress, uint16_t nMemAddr
 
     char cmd[2] = {(char)(startAddress >> 8), (char)(startAddress & 0xFF)};
     
+    printf("bcm2835_i2c_setSlaveAddress\n");
     bcm2835_i2c_setSlaveAddress(slaveAddr);
 
     char buf[1664];
