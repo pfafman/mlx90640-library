9d6da9864c6984798be23f4c1fd548429d44f15f
Timothy Pfafman <tim@pfafman.com>
2018-10-29 04:54:25 -0600

    return ta

--- mlxLibTest.py
+++ mlxLibTest.py
@@ -13,13 +13,13 @@ print(mlxLib.initAll())
 print("resolution", mlxLib.getResolution(MLX_I2C_ADDR))
 print("refresh rate:", mlxLib.getRefreshRate(MLX_I2C_ADDR))
 
-ta = c_float(0.0)
+ta = c_float()
 image = (c_float*768)()
 print(list(image))
 
 mlxLib.getTemperatureImage.argtypes = [c_float, POINTER(c_float), POINTER(c_float)]
 
 while (True):
-  code = mlxLib.getTemperatureImage(1.0, ta, image)
-  print("Code %d Ta %f" % (code,ta))
+  code = mlxLib.getTemperatureImage(1.0, byref(ta), image)
+  print("Code %d Ta %f" % (code,ta.value))
   print(list(image))
